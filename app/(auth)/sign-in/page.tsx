c12928afe5f5fec7e2dbe3fc4a4efa983965ca9c
Scott Larry <scottlarryzonto@gmail.com>
2024-11-02 21:06:19 -0700

    Look up here for chages of dataset

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,10 +1,11 @@
+import AuthForm from '@/components/AuthForm'
 import React from 'react'
 
 const SignIn = () => {
   return (
-    <div>
-      SingIn
-    </div>
+    <section className='flex-conter size-full max-sm:px-6'>
+      <AuthForm type='sign-in'/>
+    </section>
   )
 }
 
